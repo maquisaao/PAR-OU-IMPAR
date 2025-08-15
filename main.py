4ab67906fd668c1f893b59e1efabbb7b80de2053
maquisaao <maxwpinheiro@gmail.com>
2025-08-15 10:27:04 -0300

    receber numero do usuario

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 # dizer se numero é par ou impar 
+
 # receber um numero inteiro do usuario
+numero = int(input("Digite um número inteiro: "))
+
 # verificar se é par ou impar (divide por 2 e verifica o resto)
 # imprimir o resultado
+
