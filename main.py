229788da1b553034fb59967cca2df33fb0c8dabe
maquisaao <maxwpinheiro@gmail.com>
2025-08-15 10:29:46 -0300

    verificar resto da divisao por 2

--- main.py
+++ main.py
@@ -4,5 +4,9 @@
 numero = int(input("Digite um número inteiro: "))
 
 # verificar se é par ou impar (divide por 2 e verifica o resto)
+if numero % 2 == 0:
+    resultado = "par"       
+else:
+    resultado = "ímpar"
 # imprimir o resultado
 
