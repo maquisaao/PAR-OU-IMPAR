6bb1d7bde4d148ac86c51f82f71e90ee66026e06
maquisaao <maxwpinheiro@gmail.com>
2025-08-16 14:20:00 -0300

    fix: corrigir travamento ao inserir zero como número

--- main.py
+++ main.py
@@ -3,6 +3,11 @@
 # receber um numero inteiro do usuario
 numero = int(input("Digite um número inteiro: "))
 
+# trava para caso o usuario digite 0
+while numero == 0:
+    print("O número não pode ser zero. Tente novamente.")
+    numero = int(input("Digite um número inteiro: "))
+    
 # verificar se é par ou impar (divide por 2 e verifica o resto)
 if numero % 2 == 0:
     resultado = "par"       
